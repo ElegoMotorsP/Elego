5184a5ed78c650a27cb452326de7936cbecd43f0
ElegoMotors <rajesh.bhagia@lnsinfusion.com>
2025-10-08 22:59:02 +0530

    added new module

---
+++ numbertotext/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name':'Number to Text Conversation',
+    'version':'1.0',
+    'summary': 'Conversation of number to text',
+    'sequence':'1',
+    'description':'Conversation of number to text desc',
+    'author': 'prakash gatade',
+    'company': 'lnsInfusion',
+    'website':'www.website.com',
+    'category':'Accounts',
+    'depends':['sale','purchase','account'],
+    'data': [
+    "views/models_views.xml",
+    "views/reports.xml"
+    ],
+    'license':'LGPL-3',
+    'installable':True,
+    'application':True,
+    'auto_install':False,
+}
