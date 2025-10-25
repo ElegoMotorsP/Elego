378ba5349ee51ba828ad141b44e711f2e3a6ef72
ElegoMotors <rajesh.bhagia@lnsinfusion.com>
2025-10-26 03:01:43 +0530

    fixed invoice report

---
+++ custom_invoice_report/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Custom Invoice Report",
+    'version': '18.0.1.0.0',
+    'summary': "Custom QWeb Report for Sales Invoice with specific Elego sformat.",
+    'depends': ['account'],
+    'data': [
+        'reports/reports.xml',
+        'reports/report_invoice_document_format.xml',
+        'reports/report_invoice_document.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
