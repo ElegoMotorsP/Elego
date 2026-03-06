d52de2dbc071648a02356d346fa51b8b1a454b1e
haridas <haridas@gmail.com>
2026-03-06 20:26:04 +0530

    Fix group

--- custom_modules/elegomotors_setup/models/mrp_production.py
+++ custom_modules/elegomotors_setup/models/mrp_production.py
@@ -7,6 +7,6 @@ class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
     def button_mark_done(self):
-        if not self.user_has_groups('elegomotors_setup.group_manufacturing_operator'):
+        if not self.env.user.has_group('elegomotors_setup.group_manufacturing_operator'):
             raise AccessError('Only Manufacturing Operators can mark Manufacturing Orders as done.')
         return super().button_mark_done()
